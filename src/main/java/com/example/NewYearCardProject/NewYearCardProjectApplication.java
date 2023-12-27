65d745608a56d85fc3f3093ab2922c1451f6e910
Yoon321 <tkdwls734@gmail.com>
2023-12-27 18:36:16 +0900

    [feat] 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    
    * spring web
    * lombok
    * openFeign

---
+++ src/main/java/com/example/NewYearCardProject/NewYearCardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewYearCardProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewYearCardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewYearCardProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NewYearCardProject/NewYearCardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewYearCardProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewYearCardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
